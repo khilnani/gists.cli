a38826dd1e754376b9203546e92f4bee324c9602
khilnani <nik@khilnani.org>
2013-09-29 18:50:18 -0400

    Moved actions to actions.py from gists.py

--- gists_cli/gists.py
+++ gists_cli/gists.py
@@ -1,85 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os.path, rest, log, util
-
-#-------------------------------------------
-
-def list ():
-  rest.getCredentials()
-  log.debug ("Command: List.")
-
-  url = "/gists"
-  gists = rest.get(url)
-  public_count = 0
-  private_count = 0
-  print '{0:4} {1:30} {2:8} {3:25} {4}'.format('', 'Files', 'Public', 'Id', 'Description')
-  print '{0:4} {1:30} {2:8} {3:25} {4}'.format('', '-----', '------', '--', '-----------')
-  for (i, gist) in enumerate(gists):
-    private = False
-    file_list = ''
-    for (file, data) in gist['files'].items():
-      file_list += file 
-    if gist['public']:
-      public_count += 1
-    else:
-      private_count += 1
-    print '{0:4} {1:30} {2:8} {3:25} {4}'.format(i+1, file_list, str(gist['public']), gist['id'], gist['description'])
-  print ''
-  print "     You have %i Gists. (%i Private)" % (len(gists), private_count)
-
-#-------------------------------------------
-
-def create (public=False,content=None,filename=None):
-  rest.getCredentials()
-  log.debug ("Command: Create: " + str(public) + ", " + str(filename) + ", " + str(content))
-
-#-------------------------------------------
-
-def update (id):
-  rest.getCredentials()
-  log.debug ("Command: Update" + id)
-
-#-------------------------------------------
-
-def append (id):
-  rest.getCredentials()
-  log.debug ("Command: Append" + id)
-
-#-------------------------------------------
-
-def delete (id):
-  rest.getCredentials()
-  log.debug ("Command: Delete" + id)
-
-#-------------------------------------------
-
-def get_gist(id):
-  rest.getCredentials()
-  log.debug ("Internal: get_gist: " + id) 
-
-  url = "/gists/" + id
-  gist = rest.get(url)
-  return gist
-
-#-------------------------------------------
-
-def view (id):
-  log.debug ("Command: View: " + id)
-  gist = get_gist(id)
-  for (file, data) in gist['files'].items():
-    content = data['content']
-    util.line()
-    print 'Gist: {:25} File: {}'.format(id, file)
-    util.line('START')
-    print content
-    util.line('END')
-
-#-------------------------------------------
-
-def get (id, path):
-  log.debug ("Get: %s, %s" % (id, path))
-  gist = get_gist(id)
-  print "Downloading Gist %s files to '%s'" % (id, path)
+import sys, actions, log, util
 
 #-------------------------------------------
 
@@ -103,7 +24,7 @@ def main ( ):
     list
   """
   if len(args) == 0:
-    list()
+    actions.list()
   else:
     """---------------------------------------
     If args > 1, args[0] is the command. We remove it from the list
@@ -122,12 +43,11 @@ def main ( ):
     """
     if len(args) == 0:
       if cmd in ("new", "n", "create", "c"):
-        create()  # Create with no args
+        actions.create()  # Create with no args
       elif cmd in ("token", "t"):
-        rest.updateCredentials()
-        sys.exit(0)
+        actions.updateCredentials()
       else:
-        view( cmd )  # if there are no args and the cmd doesnt match, assume the cmd is the gist id
+        actions.view( cmd )  # if there are no args and the cmd doesnt match, assume the cmd is the gist id
     else:
       """ -----------------------------------------
       cmd args = 1+:
@@ -145,26 +65,27 @@ def main ( ):
         if len(args) == 1:
           # check if the arg is a Boolean, File or Content
           if util.parseBool(args[0]) != None:
-            create( public = util.parseBool(args[0]) )
+            actions.create( public = util.parseBool(args[0]) )
           elif util.isFile(args[0]) == True:
-            create( filename = args[0] )
+            actions.create( filename = args[0] )
           else:
-            create( content = args[0] )
+            actions.create( content = args[0] )
         elif len(args) > 1: 
           # args could be boolean and File, or Boolean and Content
-          create( )
+          actions.create( )
       elif cmd in ("append", "a"):
-        append( args[0] )
+        actions.append( args[0] )
       elif cmd in ("update", "u"):
-        update( args[0] )
+        actions.update( args[0] )
       elif cmd in ("delete", "d"):
-        delete( args[0] )
+        actions.delete( args[0] )
       else:
         # No match = get
-        get( cmd, args[0] ) # if no match, cmd has the ID, arg[0] has the path
+        actions.get( cmd, args[0] ) # if no match, cmd has the ID, arg[0] has the path
 
   log.debug ("Done.")
   print ''
+
 #-------------------------------------------
 
 if __name__ == "__main__":
